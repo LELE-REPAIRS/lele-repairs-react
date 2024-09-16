9e346dcbf415e4de8c73e1792a9adf372490d578
andrealtobelli <andrerogerio03@gmail.com>
2024-09-16 10:24:50 -0300

    Aicionado ForgotPassword com um layout basico e estilização.

--- src/routes/ForgotPassword/index.tsx
+++ src/routes/ForgotPassword/index.tsx
@@ -1,9 +1,21 @@
-function ForgotPassword() {
+
+import Logo from "../../components/Globais/Logo";
+import Header from "../../components/Header/Header";
+
+function ForgotPassword(){
     
-    document.title = "Lele's Repairs - Esqueci a Senha";
+    document.title = "Lele's Repairs - Esqueci a Senha ";
     
     return (
-      <></>  
+        <Header>
+            <main className="flex flex-row justify-center items-center drop-shadow-row">
+                <section className="w-[328px] h-full rounded-[30px] border-[2px] border-color_6 bg-color_2">
+                    <div className="w-full h-full inline-flex flex-col items-center p-4 gap-4">
+                        <Logo/>
+                    </div>
+                </section>
+            </main>
+        </Header>
     );
 }
 
