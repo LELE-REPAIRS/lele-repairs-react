ee162d515d9013bb3c9a5466b5a02026d25483a7
P3terHenry <pedro.antonieti@outlook.com>
2024-09-16 09:26:10 -0300

    Criação do componente Logo para reutilização em diversas páginas.

--- src/routes/Login/index.tsx
+++ src/routes/Login/index.tsx
@@ -1,9 +1,14 @@
+import Header from "../../components/Header/Header";
+
 function Login() {
     
     document.title = "Lele's Repairs - Login";
     
     return (
-      <></>  
+      <Header>
+        <main className="flex flex-row justify-center items-center">
+        </main>
+      </Header>
     );
 }
 
