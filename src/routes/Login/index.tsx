6a2495970eda14e19a359699fab7394dc807f185
P3terHenry <pedro.antonieti@outlook.com>
2024-09-16 10:44:34 -0300

    Inserção da tag button para exibir o botão de enviar e Link para rotas de Esqueci a senha e Primeiro acesso.

--- src/routes/Login/index.tsx
+++ src/routes/Login/index.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+import Checkmark from "../../components/Globais/Checkmark";
 import Input from "../../components/Globais/Input";
 import Logo from "../../components/Globais/Logo";
 import Header from "../../components/Header/Header";
@@ -15,6 +17,10 @@ function Login() {
               <form className="w-72h-full flex flex-col items-center gap-3" action="/assistentes" method="get">
                 <Input icon="bg-icon-email" classname={'w-72 h-12 p-3 pl-10 font-normal text-sm box-border rounded-full border-2 border-color_7 bg-color_3 focus:outline-color_9 drop-shadow-row placeholder-color_9 text-color_9'} type="email" placeholder="E-mail" name={"txtEmail"} id={'email'} required={true}/>
                 <Input icon="bg-icon-password" classname={'w-72 h-12 p-3 pl-10 font-normal text-sm box-border rounded-full border-2 border-color_7 bg-color_3 focus:outline-color_9 drop-shadow-row placeholder-color_9 text-color_9'} type="password" placeholder="Senha" name={"txtSenha"} id={'senha'} minlength={8} maxlength={16} required={true}/>
+                <Checkmark name={'forgot-password'} id={'forgot-password'} required={false} value={'Lembrar senha'}/>
+                <button className="flex items-center justify-center gap-2.5 h-11 px-11 rounded-full border-2 border-color_6 bg-color_3 shadow-button font-semibold text-color_11 cursor-pointer hover:bg-color_4" id="button-submit" type="submit">Entrar</button>
+                <Link className="font-normal leading-normal text-xs underline text-color_9" to="/forgot-password">Esqueci a senha {'>'}</Link>
+                <Link className="font-normal leading-normal text-xs underline text-color_9" to="/cadastro">Primeiro acesso {'>'}</Link>
               </form>
             </div>
           </section>
