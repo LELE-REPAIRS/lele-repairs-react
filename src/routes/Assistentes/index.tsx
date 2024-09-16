32df136307a363869607cc3e3a882990159d12b3
P3terHenry <pedro.antonieti@outlook.com>
2024-09-16 10:06:26 -0300

    Realizado a inserção dos Chats Ativos com o componente ChatActive passando as informações via props.

--- src/routes/Assistentes/index.tsx
+++ src/routes/Assistentes/index.tsx
@@ -1,3 +1,4 @@
+import ChatActive from "../../components/Assistentes/ChatActive";
 import Avatar from "../../components/Globais/Avatar";
 import Input from "../../components/Globais/Input";
 import MenuList from "../../components/Globais/MenuList";
@@ -32,7 +33,16 @@ function Assistentes() {
               <Input icon="bg-icon-search" classname={'w-[340px] h-14 p-4 pl-12 font-normal text-sm box-border rounded-2xl border-2 border-color_7 bg-color_3 focus:outline-color_9 drop-shadow-row placeholder-color_9 text-color_9'} type="text" placeholder="Buscar..." name={"txtBuscar"} id={'buscar'} required={false}/>
             </form>
             <div className="w-[340px] h-[670px] bg-white rounded-2xl border-2 border-neutral-200 flex p-3 flex-col gap-5 overflow-x-hidden shadow-bg_gray">
-              
+              <ChatActive name={"Diogo Albuquerque"} categoria={"Óleo do Motor"} last_message={"Ótimo. Com base nas informações passadas por você, consegui identificar que seu carro pode estar com o óleo do motor vencido. A vibração que você sente acontece por atrito entre os pistões, anéis aumentam considerávelmente."} last_message_date={"15h atrás"} avatar={"/bot-1.svg"}/>
+              <ChatActive name={"Alexandre Castilho"} categoria={"Fluído de Freio"} last_message={"Última menssagem"} last_message_date={"10h atrás"} avatar={"/bot-2.svg"}/>
+              <ChatActive name={"Rose David"} categoria={"Rodas e Pneus"} last_message={"Última menssagem"} last_message_date={"10h atrás"} avatar={"/bot-8.svg"}/>
+              <ChatActive name={"Teófilo"} categoria={"Pastilhas"} last_message={"Última menssagem"} last_message_date={"10h atrás"} avatar={"/bot-4.svg"}/>
+              <ChatActive name={"Ayxa Sam"} categoria={"Filtro de Ar"} last_message={"Última menssagem"} last_message_date={"10h atrás"} avatar={"/bot-9.svg"}/>
+              <ChatActive name={"Samira Alencar"} categoria={"Bateria"} last_message={"Última menssagem"} last_message_date={"10h atrás"} avatar={"/bot-7.svg"}/>
+              <ChatActive name={"Caio Vinícius"} categoria={"Vela de Ingnição"} last_message={"Última menssagem"} last_message_date={"10h atrás"} avatar={"/bot-6.svg"}/>
+              <ChatActive name={"Victor Miranda"} categoria={"Correira Dentada"} last_message={"Última menssagem"} last_message_date={"10h atrás"} avatar={"/bot-3.svg"}/>
+              <ChatActive name={"Vinícius Silva"} categoria={"Filtro do Combustível"} last_message={"Última menssagem"} last_message_date={"10h atrás"} avatar={"/bot-5.svg"}/>
+              <ChatActive name={"José Almeida"} categoria={"Água do Radiador"} last_message={"Última menssagem"} last_message_date={"10h atrás"} avatar={"/bot-10.svg"}/>
             </div>
           </section>
           <section className="w-[667px] h-[848px] flex-col items-start gap-[25px] inline-flex">
