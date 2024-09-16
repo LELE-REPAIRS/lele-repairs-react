fb0d214233eb23e642b3ce16d9ecc7f0364f6487
P3terHenry <pedro.antonieti@outlook.com>
2024-09-16 10:30:09 -0300

    Criação da estrutura básica do forms do Login.

--- src/routes/Login/index.tsx
+++ src/routes/Login/index.tsx
@@ -7,6 +7,12 @@ function Login() {
     return (
       <Header>
         <main className="flex flex-row justify-center items-center">
+          <section className="w-[328px] h-full rounded-[30px] border-[2px] border-color_6 bg-color_2">
+            <div className="w-full h-full inline-flex flex-col items-center p-4 gap-4">
+              <form className="w-72h-full flex flex-col items-center gap-3" action="/assistentes" method="get">
+              </form>
+            </div>
+          </section>
         </main>
       </Header>
     );
