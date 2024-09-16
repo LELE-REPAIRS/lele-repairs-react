dde4d6e605e0fb688acf3b2ebb599cfa9a054a37
andrealtobelli <andrerogerio03@gmail.com>
2024-09-16 10:26:21 -0300

    Adicionado conteúdo do forgotpassword.

--- src/routes/ForgotPassword/index.tsx
+++ src/routes/ForgotPassword/index.tsx
@@ -1,4 +1,5 @@
 
+import { Link } from "react-router-dom";
 import Logo from "../../components/Globais/Logo";
 import Header from "../../components/Header/Header";
 
@@ -12,6 +13,11 @@ function ForgotPassword(){
                 <section className="w-[328px] h-full rounded-[30px] border-[2px] border-color_6 bg-color_2">
                     <div className="w-full h-full inline-flex flex-col items-center p-4 gap-4">
                         <Logo/>
+                        <div className="w-72 h-full flex flex-col items-center gap-4">
+                            <p className="font-normal leading-normal text-color_8 text-sm text-justify">Para recuperar a sua senha, informe seu endereço de e-mail que nós exibiremos a sua senha cadastrada!</p>
+                            
+                            <Link className="font-normal leading-normal text-xs underline text-color_9" to="/">Voltar {'>'}</Link>
+                        </div>  
                     </div>
                 </section>
             </main>
