9276bcc4ec1c1e9741614fe1612b8bebbf3d0751
P3terHenry <pedro.antonieti@outlook.com>
2024-09-16 09:24:44 -0300

    Criação do componente pai Header para reutilização em todas as páginas.

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+type HeaderProps = {
+    children: React.ReactNode
+}
+
+function Header({children}: HeaderProps) {
+    return (
+        <header className="w-screen h-screen flex flex-row justify-center items-center">
+            <div className="flex flex-row justify-center items-center drop-shadow-row gap-5">
+                {children}
+            </div>
+        </header>
+    );
+}
+
+export default Header;
