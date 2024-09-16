9c67cb34d02f3f0d1d70c9d37a8def4b3dacf1ca
P3terHenry <pedro.antonieti@outlook.com>
2024-09-16 10:56:32 -0300

    Criado estrutura básica da página de cadastro.

--- src/routes/Register/index.tsx
+++ src/routes/Register/index.tsx
@@ -1,9 +1,16 @@
 function Register() {
     
-    document.title = "Lele's Repairs - Login";
+    document.title = "Lele's Repairs - Cadastro";
     
     return (
-      <></>  
+      <div className="w-screen h-screen flex flex-row justify-center items-center">
+        <main className="flex flex-row justify-center items-center drop-shadow-row">
+          <section className="w-[328px] h-full rounded-[30px] border-[2px] border-color_6 bg-color_2">
+            <div className="w-full h-full inline-flex flex-col items-center p-4 gap-4">
+            </div>
+          </section>
+        </main>
+      </div>
     );
 }
 
